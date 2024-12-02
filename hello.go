216cf0086ff44634e86e1a1ce7b47a47a8e7534d
lucasoneves <casluhc@gmail.com>
2024-12-02 17:23:49 -0300

    Estruturas condicionais

--- hello.go
+++ hello.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	userName := "Lucas"
+	version := 1.1
+	fmt.Println("Olá, sr.", userName, version)
+	fmt.Println("Este programa está na versão", version)
+
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("Comando escolhido:", comando)
+
+	if comando == 0 {
+
+	} else if comando == 1 {
+		fmt.Println("Monitorando...")
+	} else if comando == 2 {
+		fmt.Println("Exibindo logs...")
+	} else if comando == 0 {
+		fmt.Println("Saindo do programa.")
+	} else {
+		fmt.Println("Não conheço esse código")
+	}
 }
