1453adbbcc586fb6a8a5661754043e009a7c2b57
lucasoneves <casluhc@gmail.com>
2024-12-04 17:22:57 -0300

    Intro to functions

--- hello.go
+++ hello.go
@@ -1,22 +1,14 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
-	userName := "Lucas"
-	version := 1.1
-	var comando int
 
-	fmt.Println("Olá, sr.", userName)
-	fmt.Println("Este programa está na versão", version)
+	showIntro()
+	showMenu()	
 
-	fmt.Println("1 - Iniciar monitoramento")
-	fmt.Println("2 - Exibir logs")
-	fmt.Println("0 - Sair do programa")
-
-	fmt.Scan(&comando)
-
-	fmt.Println("Comando escolhido:", comando)
+	comando := readCommand()
 
 	switch comando {
 	case 1:
@@ -25,7 +17,31 @@ func main() {
 		fmt.Println("Exibindo logs...")
 	case 0:
 		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+
 	default:
 		fmt.Println("Não conheço esse comando...")
+		os.Exit(-1)
 	}
+}
+
+func showIntro() {
+	userName := "Lucas"
+	version := 1.1
+	fmt.Println("Olá, sr.", userName)
+	fmt.Println("Este programa está na versão", version)
+}
+
+func showMenu() {
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+}
+
+func readCommand() int {
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("Comando escolhido:", comando)
+
+	return comando
 }
