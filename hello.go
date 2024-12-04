0a861047acbf6e5c8cc6209d250001943ab3ed3f
lucasoneves <casluhc@gmail.com>
2024-12-04 17:12:29 -0300

    Flow controll with switch

--- hello.go
+++ hello.go
@@ -5,26 +5,27 @@ import "fmt"
 func main() {
 	userName := "Lucas"
 	version := 1.1
-	fmt.Println("Olá, sr.", userName, version)
+	var comando int
+
+	fmt.Println("Olá, sr.", userName)
 	fmt.Println("Este programa está na versão", version)
 
 	fmt.Println("1 - Iniciar monitoramento")
 	fmt.Println("2 - Exibir logs")
 	fmt.Println("0 - Sair do programa")
 
-	var comando int
 	fmt.Scan(&comando)
+
 	fmt.Println("Comando escolhido:", comando)
 
-	if comando == 0 {
-		fmt.Println("Saindo do programa...")
-	} else if comando == 1 {
+	switch comando {
+	case 1:
 		fmt.Println("Monitorando...")
-	} else if comando == 2 {
+	case 2:
 		fmt.Println("Exibindo logs...")
-	} else if comando == 0 {
-		fmt.Println("Saindo do programa.")
-	} else {
-		fmt.Println("Não conheço esse código")
+	case 0:
+		fmt.Println("Saindo do programa...")
+	default:
+		fmt.Println("Não conheço esse comando...")
 	}
 }
