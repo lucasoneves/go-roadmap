ae3e7e431b06c0e72eeed64bc611e17d4b07d437
lucasoneves <casluhc@gmail.com>
2024-12-09 20:04:01 -0300

    Formatação de data usando o pacote Time

--- hello.go
+++ hello.go
@@ -128,7 +128,7 @@ func registraLog(site string, status bool) {
 		fmt.Println("Error ocurred", err)
 	}
 
-	arquivo.WriteString(site + " - online: " + strconv.FormatBool(status) + "\n")
+	arquivo.WriteString(time.Now().Format("02/01/2006 - 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
 }
